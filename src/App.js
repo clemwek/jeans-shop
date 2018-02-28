5d7ad55cca8424f990176b2c1a951c04a12530a6
dhywty <clemwek@gmail.com>
2018-02-28 14:03:58 +0300

    Add material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'material-ui/Button';
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +14,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Button variant="raised" color="primary">
+            Hello World
+          </Button>
         </p>
       </div>
     );
