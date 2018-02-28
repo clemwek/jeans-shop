603baf0822d619bd69a5891c3014d4768c679a0f
dhywty <clemwek@gmail.com>
2018-02-28 17:23:57 +0300

    Add routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import HomePage from './components/home/home'
+import HomePage from './components/home/home';
+import Navbar from './components/common/navbar';
 
 export default () => (
   <BrowserRouter>
     <div>
+      <Route component={Navbar}></Route>
       <Route exact path="/" component={HomePage}></Route>
     </div>
   </BrowserRouter>
